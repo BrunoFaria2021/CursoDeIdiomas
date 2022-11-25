e389a250285508a7f3b45b6644fbfe4ad9c3e0ad
BrunoFaria2021 <82816648+BrunoFaria2021@users.noreply.github.com>
2022-11-25 00:48:33 -0300

    Estruturando Projeto DDD

---
+++ CursosDeIdiomas/CursosDeIdiomas.Domain/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursosDeIdiomas.Domain
+{
+    public class Aluno
+    {
+        public string Aluno { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+
+
+
+    }
+}
---
+++ CursosDeIdiomas/CursosDeIdiomas.Domain/Turma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursosDeIdiomas.Domain
+{
+    public class Turma
+    {
+        public string Numero { get; set; }
+        public string AnoLetivo { get; set; }
+
+    }
+}
