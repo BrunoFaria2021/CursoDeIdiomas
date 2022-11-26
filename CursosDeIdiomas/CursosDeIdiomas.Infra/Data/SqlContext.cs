33c720a90698d51d26bc2ee657d45d217365b6e4
BrunoFaria2021 <82816648+BrunoFaria2021@users.noreply.github.com>
2022-11-25 23:10:48 -0300

    Criando Sql context, e Interface base para repositorios

---
+++ CursosDeIdiomas/CursosDeIdiomas.Infra/Data/SqlContext.cs
@@ -0,0 +1,21 @@
+﻿using CursosDeIdiomas.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursosDeIdiomas.Infra.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public DbSet<Turma> Turmas { get; set; }
+
+    }
+
+}
