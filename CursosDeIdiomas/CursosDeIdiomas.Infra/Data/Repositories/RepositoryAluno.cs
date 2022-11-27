7fb4ec87d94cd20b57512ebcf075509d9596fe53
BrunoFaria2021 <82816648+BrunoFaria2021@users.noreply.github.com>
2022-11-27 16:04:53 -0300

    Criando codigo dentro de infra para repositorio RepositoryTurma, RepositoryAluno

---
+++ CursosDeIdiomas/CursosDeIdiomas.Infra/Data/Repositories/RepositoryAluno.cs
@@ -0,0 +1,18 @@
+﻿using CursosDeIdiomas.Domain;
+using CursosDeIdiomas.Domain.core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursosDeIdiomas.Infra.Data.Repositories
+
+public class RepositoryAluno : RepositoryBase<Aluno>, IRepositoryAluno
+{
+    private readonly SqlContext sqlContext;
+    public RepositoryAluno(SqlContext sqlContext) : base(sqlContext)
+    {
+        this.sqlContext = sqlContext;
+    }
+}
---
+++ CursosDeIdiomas/CursosDeIdiomas.Infra/Data/Repositories/RepositoryTurma.cs
@@ -0,0 +1,19 @@
+﻿using CursosDeIdiomas.Domain;
+using CursosDeIdiomas.Domain.core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursosDeIdiomas.Infra.Data.Repositories
+{
+    public class RepositoryTurma : RepositoryBase<Turma>, IRepositoryTurma
+    {
+        private readonly SqlContext sqlContext;
+        public RepositoryTurma(SqlContext sqlContext) : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;   
+        }
+    }
+}
